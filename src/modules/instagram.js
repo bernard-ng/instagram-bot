b8623f444313f3e447bdb306d9dbec7438d9e8bf
bernard-ng <ngandubernard@gmail.com>
2020-04-26 20:19:19 +0100

    feat(login): add login action to the bot

--- src/modules/instagram.js
+++ src/modules/instagram.js
@@ -7,15 +7,29 @@ const ig = {
     browser: null,
 
     async initialize() {
+        console.warn('start > launching the browser');
         ig.browser = await puppeteer.launch({ headless: false });
         ig.page = await ig.browser.newPage();
     },
 
     async login(username, password) {
-        await ig.page.goto(BASE_URL, { waitUntil: 'networkidle2' });
-        const loginButton = ig.page.$x("//a[contains(text(), 'Log In')]");
+        try {
+            console.warn('start > login');
+            await ig.page.goto(`${BASE_URL}/accounts/login`, { waitUntil: 'networkidle2', timeout: 60000 });
+            await ig.page.waitFor(2500);
 
-        console.log({ loginButton });
+            console.log('filling username name password');
+            await ig.page.type("input[name='username']", username, { delay: 50 });
+            await ig.page.type("input[name='password']", password, { delay: 70 });
+
+            console.log('Login and wait for 10s');
+            await ig.page.click("button[type='submit']");
+            await ig.page.waitForNavigation();
+            console.warn('finished > login');
+        } catch (e) {
+            console.error("Error occur while performing > Login");
+            console.log({ error: e });
+        }
     }
 }
 
